eded950268f5e41279b1ab34fabc98bd46cf19a6
abc <abc@gmail.com>
2022-05-25 18:20:56 -0300

    Ejercicio clase 8

---
+++ Clase8/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Ingrese la cantidad de alumnos por favor: ");
+int cantidadAlumnos = int.Parse(Console.ReadLine());
+int[] notas = new int[cantidadAlumnos];
+
+Console.WriteLine("Ingrese las notas de cada alumno: ");
+
+for (int posicion  = 0; posicion < notas.Length; posicion++)
+{
+    Console.WriteLine($"Notas del alumno N° {posicion+1}: ");
+    notas[posicion] = int.Parse(Console.ReadLine());
+}
