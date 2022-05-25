b966c632812dbbdea9232f44ce60381a4fffb835
abc <abc@gmail.com>
2022-05-25 19:26:02 -0300

    .

--- Clase8/Program.cs
+++ Clase8/Program.cs
@@ -5,8 +5,15 @@ int[] notas = new int[cantidadAlumnos];
 
 Console.WriteLine("Ingrese las notas de cada alumno: ");
 
-for (int posicion  = 0; posicion < notas.Length; posicion++)
+for (int posicion = 0; posicion < notas.Length; posicion++)
 {
-    Console.WriteLine($"Notas del alumno N° {posicion+1}: ");
+    Console.WriteLine($"Notas del alumno N° {posicion + 1}: ");
     notas[posicion] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine();
+Console.WriteLine("===============");
+Console.WriteLine();
+for (int posicion = 0; posicion < notas.Length; posicion++)
+{
+    Console.WriteLine($"La nota del alumno N° {posicion + 1} es {notas[posicion]}");
 }
